60e0be9083dd072c75d96efd5b3e2b0b6a9d6432
Michael Wilber <wilmic1102@gmail.com>
2015-06-18 16:05:40 +0000

    googlenet is now alexnet

--- googlenet.lua
+++ googlenet.lua
@@ -30,89 +30,140 @@ print(opt)
 
 --[[Model]]--
 
-function buildInceptionLayer(inCh, n1ch, n3reduce, n3ch, n5reduce, n5ch, poolproj)
-   local concat = nn.DepthConcat(2)
-   local branch1x1 = nn.Sequential()
-     branch1x1:add(nn.SpatialConvolution(inCh,n1ch,1,1))
-     branch1x1:add(nn.ReLU(true))
-   local branch3x3 = nn.Sequential()
-     branch3x3:add(nn.SpatialConvolution(inCh,n3reduce,1,1))
-     --branch3x3:add(nn.SpatialBatchNormalization(n3reduce))
-     branch3x3:add(nn.ReLU(true))
-     branch3x3:add(nn.SpatialConvolution(n3reduce,n3ch,3,3))
-     branch3x3:add(nn.ReLU(true))
-   local branch5x5 = nn.Sequential()
-     branch5x5:add(nn.SpatialConvolution(inCh,n5reduce,1,1))
-     --branch5x5:add(nn.SpatialBatchNormalization(n5reduce))
-     branch5x5:add(nn.ReLU(true))
-     branch5x5:add(nn.SpatialConvolution(n5reduce,n5ch,5,5))
-     --branch5x5:add(nn.SpatialBatchNormalization(n5ch))
-     branch5x5:add(nn.ReLU(true))
-   local branch3x3MP = nn.Sequential()
-     branch3x3MP:add(nn.SpatialMaxPooling(3,3, 2,2))
-     branch3x3MP:add(nn.SpatialConvolution(inCh,poolproj,1,1))
-     --branch3x3MP:add(nn.SpatialBatchNormalization(poolproj))
-     branch3x3MP:add(nn.ReLU(true))
-    concat:add(branch1x1)
-    concat:add(branch3x3)
-    concat:add(branch5x5)
-    concat:add(branch3x3MP)
-    return concat
-   -- n.b.! since the convolutions have different sizes, many of these
-   -- layers will be padded with zeros!
+-- function buildInceptionLayer(inCh, n1ch, n3reduce, n3ch, n5reduce, n5ch, poolproj)
+--    local concat = nn.DepthConcat(2)
+--    local branch1x1 = nn.Sequential()
+--      branch1x1:add(nn.SpatialConvolution(inCh,n1ch,1,1))
+--      branch1x1:add(nn.ReLU(true))
+--    local branch3x3 = nn.Sequential()
+--      branch3x3:add(nn.SpatialConvolution(inCh,n3reduce,1,1))
+--      --branch3x3:add(nn.SpatialBatchNormalization(n3reduce))
+--      branch3x3:add(nn.ReLU(true))
+--      branch3x3:add(nn.SpatialConvolution(n3reduce,n3ch,3,3))
+--      branch3x3:add(nn.ReLU(true))
+--    local branch5x5 = nn.Sequential()
+--      branch5x5:add(nn.SpatialConvolution(inCh,n5reduce,1,1))
+--      --branch5x5:add(nn.SpatialBatchNormalization(n5reduce))
+--      branch5x5:add(nn.ReLU(true))
+--      branch5x5:add(nn.SpatialConvolution(n5reduce,n5ch,5,5))
+--      --branch5x5:add(nn.SpatialBatchNormalization(n5ch))
+--      branch5x5:add(nn.ReLU(true))
+--    local branch3x3MP = nn.Sequential()
+--      branch3x3MP:add(nn.SpatialMaxPooling(3,3, 2,2))
+--      branch3x3MP:add(nn.SpatialConvolution(inCh,poolproj,1,1))
+--      --branch3x3MP:add(nn.SpatialBatchNormalization(poolproj))
+--      branch3x3MP:add(nn.ReLU(true))
+--     concat:add(branch1x1)
+--     concat:add(branch3x3)
+--     concat:add(branch5x5)
+--     concat:add(branch3x3MP)
+--     return concat
+--    -- n.b.! since the convolutions have different sizes, many of these
+--    -- layers will be padded with zeros!
+-- end
+-- 
+-- model = nn.Sequential()
+-- model:add(nn.SpatialConvolution(3,64, 7,7,2,2, 3,3)) -- 224x224 -> 112x112
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialMaxPooling(2,2,2,2)) -- 112x112 -> 56x56
+-- model:add(nn.SpatialBatchNormalization(64))
+-- model:add(nn.SpatialConvolution(64,192, 3,3,1,1, 1,1))
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialMaxPooling(2,2,2,2)) -- 56x56 -> 28x28
+-- model:add(nn.SpatialBatchNormalization(192))
+-- ---- Inception 3a
+-- model:add(buildInceptionLayer(192, 64, 96, 128, 16, 32, 32))
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialBatchNormalization(256))
+-- ---- Inception 3b
+-- model:add(buildInceptionLayer(256, 128, 128, 192, 32, 96, 64))
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialMaxPooling(2,2,2,2)) -- 28x28 -> 14x14
+-- model:add(nn.SpatialBatchNormalization(480))
+-- ---- Inception 4a
+-- model:add(buildInceptionLayer(480, 192, 96, 208, 16, 48, 64))
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialBatchNormalization(512))
+-- ---- Inception 4b
+-- model:add(buildInceptionLayer(512, 160, 112, 224, 24, 64, 64))
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialBatchNormalization(512))
+-- ---- Inception 4c
+-- model:add(buildInceptionLayer(512, 128, 128, 256, 24, 64, 64))
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialBatchNormalization(512))
+-- ---- Inception 4d
+-- model:add(buildInceptionLayer(512, 112, 144, 288, 32, 64, 64))
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialBatchNormalization(528))
+-- ---- Inception 4e
+-- model:add(buildInceptionLayer(528, 256, 160, 320, 32, 128, 128))
+-- model:add(nn.SpatialMaxPooling(2,2,2,2))
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialBatchNormalization(832))
+-- ---- Inception 5a
+-- model:add(buildInceptionLayer(832, 256, 160, 320, 32, 128, 128))
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialBatchNormalization(832))
+-- ---- Inception 5b
+-- model:add(buildInceptionLayer(832, 384, 192, 384, 48, 128, 128))
+-- model:add(nn.ReLU(true))
+-- model:add(nn.SpatialAveragePooling(7,7,1,1))
+-- model:add(nn.Dropout(0.4))
+-- model:add(nn.Collapse(3))
+-- model:add(nn.Linear(1024, 1000))
+-- model:add(nn.LogSoftMax())
+
+local features = nn.Concat(2)
+local fb1 = nn.Sequential() -- branch 1
+fb1:add(nn.SpatialConvolution(3,48,11,11,4,4,2,2))       -- 224 -> 55
+fb1:add(nn.ReLU())
+fb1:add(nn.SpatialMaxPooling(3,3,2,2))                   -- 55 ->  27
+
+fb1:add(nn.SpatialConvolution(48,128,5,5,1,1,2,2))       --  27 -> 27
+fb1:add(nn.ReLU())
+fb1:add(nn.SpatialMaxPooling(3,3,2,2))                   --  27 ->  13
+
+fb1:add(nn.SpatialConvolution(128,192,3,3,1,1,1,1))      --  13 ->  13
+fb1:add(nn.ReLU())
+
+fb1:add(nn.SpatialConvolution(192,192,3,3,1,1,1,1))      --  13 ->  13
+fb1:add(nn.ReLU())
+
+fb1:add(nn.SpatialConvolution(192,128,3,3,1,1,1,1))      --  13 ->  13
+fb1:add(nn.ReLU())
+
+fb1:add(nn.SpatialMaxPooling(3,3,2,2))                   -- 13 -> 6
+
+local fb2 = fb1:clone() -- branch 2
+for k,v in ipairs(fb2:findModules('nn.SpatialConvolution')) do
+   v:reset() -- reset branch 2's weights
 end
 
+features:add(fb1)
+features:add(fb2)
+
+-- 1.3. Create Classifier (fully connected layers)
+local classifier = nn.Sequential()
+classifier:add(nn.Copy(nil, nil, true)) -- prevents a newContiguous in SpatialMaxPooling:backward()
+classifier:add(nn.View(256*6*6))
+classifier:add(nn.Dropout(0.5))
+classifier:add(nn.Linear(256*6*6, 4096))
+classifier:add(nn.Threshold(0, 1e-6))
+
+classifier:add(nn.Dropout(0.5))
+classifier:add(nn.Linear(4096, 4096))
+classifier:add(nn.Threshold(0, 1e-6))
+
+classifier:add(nn.Linear(4096, 1000))
+classifier:add(nn.LogSoftMax())
+
+-- 1.4. Combine 1.1 and 1.3 to produce final model
 model = nn.Sequential()
-model:add(nn.SpatialConvolution(3,64, 7,7,2,2, 3,3)) -- 224x224 -> 112x112
-model:add(nn.ReLU(true))
-model:add(nn.SpatialMaxPooling(2,2,2,2)) -- 112x112 -> 56x56
-model:add(nn.SpatialBatchNormalization(64))
-model:add(nn.SpatialConvolution(64,192, 3,3,1,1, 1,1))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialMaxPooling(2,2,2,2)) -- 56x56 -> 28x28
-model:add(nn.SpatialBatchNormalization(192))
----- Inception 3a
-model:add(buildInceptionLayer(192, 64, 96, 128, 16, 32, 32))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialBatchNormalization(256))
----- Inception 3b
-model:add(buildInceptionLayer(256, 128, 128, 192, 32, 96, 64))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialMaxPooling(2,2,2,2)) -- 28x28 -> 14x14
-model:add(nn.SpatialBatchNormalization(480))
----- Inception 4a
-model:add(buildInceptionLayer(480, 192, 96, 208, 16, 48, 64))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialBatchNormalization(512))
----- Inception 4b
-model:add(buildInceptionLayer(512, 160, 112, 224, 24, 64, 64))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialBatchNormalization(512))
----- Inception 4c
-model:add(buildInceptionLayer(512, 128, 128, 256, 24, 64, 64))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialBatchNormalization(512))
----- Inception 4d
-model:add(buildInceptionLayer(512, 112, 144, 288, 32, 64, 64))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialBatchNormalization(528))
----- Inception 4e
-model:add(buildInceptionLayer(528, 256, 160, 320, 32, 128, 128))
-model:add(nn.SpatialMaxPooling(2,2,2,2))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialBatchNormalization(832))
----- Inception 5a
-model:add(buildInceptionLayer(832, 256, 160, 320, 32, 128, 128))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialBatchNormalization(832))
----- Inception 5b
-model:add(buildInceptionLayer(832, 384, 192, 384, 48, 128, 128))
-model:add(nn.ReLU(true))
-model:add(nn.SpatialAveragePooling(7,7,1,1))
-model:add(nn.Dropout(0.4))
-model:add(nn.Collapse(3))
-model:add(nn.Linear(1024, 1000))
-model:add(nn.LogSoftMax())
+model:add(nn.Convert(),1)
+model:add(features)
+model:add(classifier)
+
 loss = nn.ClassNLLCriterion()
 
 --[[data]]--
