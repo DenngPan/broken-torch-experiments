2b117c533e616e05658ed7b423da03a07b16a815
Michael Wilber <wilmic1102@gmail.com>
2015-06-25 13:19:08 +0000

    fix alexnet script

--- alexnet.lua
+++ alexnet.lua
@@ -3,8 +3,8 @@ require 'optim'
 require 'cunn'
 require 'TrainHelpers'
 require 'cutorch'
-require 'loadcaffe'
 require 'cudnn'
+require 'inn'
 
 ------------ Model ------------
 
@@ -72,14 +72,21 @@ local preprocess = TrainHelpers.normalizePreprocessDataset(ds_train, 255)
 local ds_val = ds_all:loadValid()
 local sampler = dp.RandomSampler{
     batch_size = 128,
-    epoch_size = 100000,
+    epoch_size = 1000,
     ppf = preprocess
 }
 ds_train:multithread(4)
+ds_val:multithread(4)
 sampler:async()
+local val_sampler = dp.RandomSampler{ batch_size = 12, ppf = preprocess,
+    epoch_size = 8000
+}
+val_sampler:async()
 
 
 ------------ Actual Training ------------
+
+local weights, gradients = model:getParameters()
 
 function forwardBackwardBatch(inputs, targets)
    model:training()
@@ -96,6 +103,7 @@ function forwardBackwardBatch(inputs, targets)
 end
 
 local lossLog = {}
+local accuracyLog = {}
 local epochCounter = 0
 local sgdState = {
    learningRate = 0.01,
@@ -104,12 +112,13 @@ local sgdState = {
    weightDecay  = 0.0005,
    nesterov     = true
 }
-while true; do -- Each epoch
+while true do -- Each epoch
    epochCounter = epochCounter + 1
    local epoch = sampler:sampleEpoch(ds_train)
    local batch,imagesSeen,epochSize
    print("---------- Epoch "..epochCounter.." ----------")
-   while true; do -- Each batch
+   while true do -- Each batch
+      collectgarbage(); collectgarbage()
       batch,imagesSeen,epochSize = epoch(batch)
       if not batch then
          break
@@ -125,18 +134,23 @@ while true; do -- Each epoch
                 sgdState)
       -- Display progress and loss
       xlua.progress(imagesSeen, epochSize)
-      if (sgdState.evalCounter / sampler:batchSize()) % 20 == 0 then
-         print("Loss:", loss_val)
+      if sgdState.evalCounter % 20 == 0 then
+         print("\027[KBatch "..sgdState.evalCounter.." loss:", loss_val)
       end
       table.insert(lossLog, loss_val)
    end
    -- Epoch completed! Snapshot model.
-   torch.save("snapshots/alexnet-epoch"..epochCounter..".t7",
-              {model=TrainHelpers.sanitizeModel(model),
-               sgdState=sgdState,
-               lossLog=lossLog})
+   torch.save("snapshots/alexnet-epoch"..epochCounter..".t7", {
+       model = TrainHelpers.sanitizeModel(model),
+       sgdState = sgdState,
+       lossLog = lossLog,
+       epochCounter = epochCounter
+   })
    -- Evaluate model
-   TrainHelpers.evaluateModel(model, sampler:sampleEpoch(ds_val), true)
+   table.insert(accuracyLog, {
+       epochCounter = epochCounter,
+       results = TrainHelpers.evaluateModel(model, val_sampler:sampleEpoch(ds_val), true)
+   })
    -- Every so often, decrease learning rate
    if epochCounter % 20 == 0 then
       sgdState.learningRate = sgdState.learningRate * 0.1
